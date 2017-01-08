4bc066aff333555bdb08a2d68b9d6d09afba27e7
samjbro <sjbroughton1@gmail.com>
2017-01-08 13:02:52 +0000

    adds seed data and basic table styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.delete_all
+
+Item.create!(name: 'Smart Hub', price: 49.99)
+Item.create!(name: 'Motion Sensor', price: 24.99)
+Item.create!(name: 'Wireless Camera', price: 99.99)
+Item.create!(name: 'Smoke Sensor', price: 19.99)
+Item.create!(name: 'Water Leak Sensor', price: 14.99)
